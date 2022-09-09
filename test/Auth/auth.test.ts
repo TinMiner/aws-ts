a0aa2f46c548c38d7c9c4225ff1fd0e900b12d51
TinMiner <alan.beecroft@talk21.com>
2022-09-09 14:14:25 +0100

    initial commit

---
+++ test/Auth/auth.test.ts
@@ -0,0 +1,16 @@
+import { AuthService } from './AuthService';
+import { config } from './config';
+import * as AWS from 'aws-sdk';
+
+
+
+async function callStuff() {
+    const authService = new AuthService();
+
+    const user = await authService.login(config.TEST_USER_NAME, config.TEST_USER_PASSWORD);
+    await authService.getAWSTemporaryCreds(user);
+    const someCreds = AWS.config.credentials;
+    const a = 5;
+}
+
+callStuff();
